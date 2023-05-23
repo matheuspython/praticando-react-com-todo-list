3542adfd78341513806a5f7dd2fe0be02b32e0c7
matheus souza <64442354+matheuspython@users.noreply.github.com>
2023-05-23 09:07:48 -0300

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,21 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import { Card } from './components/Card'
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <div className='app'>
+      <div>
+        <input type="text" placeholder='fazer café' />
+        <button>Adicionar</button>
+      </div>
+      <Card title="fazer café" />
+    </div>
+  )
+}
+
+export default App
